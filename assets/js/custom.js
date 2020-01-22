c53cf27de9bc35c9c46ae3ee914dc826c4269505
Luka <luka@mindfuel.dev>
2020-01-22 16:30:41 +0100

    initial site structure

---
+++ assets/js/custom.js
@@ -0,0 +1,262 @@
+jQuery( document ).ready(function( $ ) {
+
+
+	"use strict";
+
+
+    
+        $(function() {
+            $( "#tabs" ).tabs();
+        });
+
+
+        // Page loading animation
+
+        $("#preloader").animate({
+            'opacity': '0'
+        }, 600, function(){
+            setTimeout(function(){
+                $("#preloader").css("visibility", "hidden").fadeOut();
+            }, 300);
+        });
+        
+
+        $(window).scroll(function() {
+          var scroll = $(window).scrollTop();
+          var box = $('.header-text').height();
+          var header = $('header').height();
+
+          if (scroll >= box - header) {
+            $("header").addClass("background-header");
+          } else {
+            $("header").removeClass("background-header");
+          }
+        });
+		if ($('.owl-testimonials').length) {
+            $('.owl-testimonials').owlCarousel({
+                loop: true,
+                nav: false,
+                dots: true,
+                items: 1,
+                margin: 30,
+                autoplay: false,
+                smartSpeed: 700,
+                autoplayTimeout: 6000,
+                responsive: {
+                    0: {
+                        items: 1,
+                        margin: 0
+                    },
+                    460: {
+                        items: 1,
+                        margin: 0
+                    },
+                    576: {
+                        items: 2,
+                        margin: 20
+                    },
+                    992: {
+                        items: 2,
+                        margin: 30
+                    }
+                }
+            });
+        }
+        if ($('.owl-partners').length) {
+            $('.owl-partners').owlCarousel({
+                loop: true,
+                nav: false,
+                dots: true,
+                items: 1,
+                margin: 30,
+                autoplay: false,
+                smartSpeed: 700,
+                autoplayTimeout: 6000,
+                responsive: {
+                    0: {
+                        items: 1,
+                        margin: 0
+                    },
+                    460: {
+                        items: 1,
+                        margin: 0
+                    },
+                    576: {
+                        items: 2,
+                        margin: 20
+                    },
+                    992: {
+                        items: 4,
+                        margin: 30
+                    }
+                }
+            });
+        }
+
+        $(".Modern-Slider").slick({
+            autoplay:true,
+            autoplaySpeed:10000,
+            speed:600,
+            slidesToShow:1,
+            slidesToScroll:1,
+            pauseOnHover:false,
+            dots:true,
+            pauseOnDotsHover:true,
+            cssEase:'linear',
+           // fade:true,
+            draggable:false,
+            prevArrow:'<button class="PrevArrow"></button>',
+            nextArrow:'<button class="NextArrow"></button>', 
+        });
+
+        function visible(partial) {
+            var $t = partial,
+                $w = jQuery(window),
+                viewTop = $w.scrollTop(),
+                viewBottom = viewTop + $w.height(),
+                _top = $t.offset().top,
+                _bottom = _top + $t.height(),
+                compareTop = partial === true ? _bottom : _top,
+                compareBottom = partial === true ? _top : _bottom;
+
+            return ((compareBottom <= viewBottom) && (compareTop >= viewTop) && $t.is(':visible'));
+
+        }
+
+        $(window).scroll(function(){
+
+          if(visible($('.count-digit')))
+            {
+              if($('.count-digit').hasClass('counter-loaded')) return;
+              $('.count-digit').addClass('counter-loaded');
+              
+        $('.count-digit').each(function () {
+          var $this = $(this);
+          jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+            duration: 3000,
+            easing: 'swing',
+            step: function () {
+              $this.text(Math.ceil(this.Counter));
+            }
+          });
+        });
+        }
+    })
+    let particlesConfig = 
+    {
+        "particles": {
+          "number": {
+            "value": 43,
+            "density": {
+              "enable": true,
+              "value_area": 552.4033491425909
+            }
+          },
+          "color": {
+            "value": "#ffffff"
+          },
+          "shape": {
+            "type": "circle",
+            "stroke": {
+              "width": 0,
+              "color": "#000000"
+            },
+            "polygon": {
+              "nb_sides": 5
+            },
+            "image": {
+              "src": "img/github.svg",
+              "width": 100,
+              "height": 100
+            }
+          },
+          "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+              "enable": false,
+              "speed": 1,
+              "opacity_min": 0.1,
+              "sync": false
+            }
+          },
+          "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+              "enable": false,
+              "speed": 40,
+              "size_min": 0.1,
+              "sync": false
+            }
+          },
+          "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+          },
+          "move": {
+            "enable": true,
+            "speed": 2,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+              "enable": false,
+              "rotateX": 600,
+              "rotateY": 1200
+            }
+          }
+        },
+        "interactivity": {
+          "detect_on": "canvas",
+          "events": {
+            "onhover": {
+              "enable": false,
+              "mode": "bubble"
+            },
+            "onclick": {
+              "enable": false,
+              "mode": "push"
+            },
+            "resize": true
+          },
+          "modes": {
+            "grab": {
+              "distance": 400,
+              "line_linked": {
+                "opacity": 1
+              }
+            },
+            "bubble": {
+              "distance": 400,
+              "size": 40,
+              "duration": 2,
+              "opacity": 8,
+              "speed": 3
+            },
+            "repulse": {
+              "distance": 200,
+              "duration": 0.4
+            },
+            "push": {
+              "particles_nb": 4
+            },
+            "remove": {
+              "particles_nb": 2
+            }
+          }
+        },
+        "retina_detect": false
+      }
+
+
+    // Luka's custom code
+    particlesJS.load('particles-js', particlesConfig, function() {
+        console.log('callback - particles.js config loaded');
+    });
+});
